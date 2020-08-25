4a6b333d073f51720d3fa6186d602e932b6b12ae
Camryn Patterson <camrynpatterson@yahoo.com>
2020-08-25 03:13:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
   end
 
 def create_an_array
-  ["Camryn", "Emma", "River", "Ashley"]
+  ["camryn", "emma", "river", "ashley"]
   end
 
 def add_element_to_end_of_array(array, element)
