75b6c1aadc6bc9a62de67b7090b32f9aee7f916a
Camryn Patterson <camrynpatterson@yahoo.com>
2020-08-25 03:16:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,12 +21,11 @@ end
 
 def remove_element_from_start_of_array(array)
   array.shift
-  
-end
+  end
 
 def retrieve_element_from_index(array, index_number)
-  
-end
+  array.[index_number]
+  end
 
 def retrieve_first_element_from_array(array)
   
